7561775824f648bd71dcbcdff8f8eb90064788b8
Zhang Qing <mixzq@outlook.com>
2022-08-22 20:45:22 +0200

    first

---
+++ node_modules/sendgrid-mail/test/test_index.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+
+// NOTE: don't use arrow functions with mocha.
+// See https://github.com/mochajs/mocha/issues/2018
+describe('Index', function () {
+  it('true === true (Replace this test with more useful ones)', function () {
+    assert.strictEqual(true, true)
+  })
+})
